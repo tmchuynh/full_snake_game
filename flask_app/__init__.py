e437f47b46d5ec9a8333a9dfff5ecf8ae4c0a52f
Tina Huynh <tinamchuynh@gmail.com>
2024-07-28 11:12:07 -0700

    button click

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,11 +3,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 app.secret_key = "randomly the kids decided that chasing the snakes would be a lot of fun"
 DATABASE = "snake_game"
--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -1,3 +1,16 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_app import app
 from flask_app.models.user_model import User
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/{id}', methods=['POST'])
+def create_new_user():
+    button_value = request.form.get('button')
+    if button_value:
+        return f'Button was clicked with value: {button_value}'
+    return 'Button was not clicked'
