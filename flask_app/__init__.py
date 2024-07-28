67f2542b803341aae2e4411c79aa55aeaa53bfcb
Tina Huynh <tinamchuynh@gmail.com>
2024-07-28 10:58:54 -0700

    debug on

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,6 @@
 # __init__.py
 from flask import Flask
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.secret_key = "randomly the kids decided that chasing the snakes would be a lot of fun"
 DATABASE = "snake_game"
