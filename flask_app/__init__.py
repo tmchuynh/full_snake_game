da0d1999b29d77c3a61a924fc4767bbaef8bec82
Tina Huynh <tinamchuynh@gmail.com>
2024-07-28 08:54:34 -0700

    update flask_app/__init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
 from flask import Flask
 app = Flask(__name__)
-app.secret_key = ""
+app.secret_key = "randomly the kids decided that chasing the snakes would be a lot of fun"
 DATABASE = "snake_game"
