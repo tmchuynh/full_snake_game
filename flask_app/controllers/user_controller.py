d321ac8f3b411dc11b8c3483e95050663633b91c
Tina Huynh <tinamchuynh@gmail.com>
2024-07-28 09:39:31 -0700

    add / route

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -1,3 +1,8 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_app import app
 from flask_app.models.user_model import User
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
